ad2015d5d6a06fdabaad6d09c9d32b04688f0251
Rohan-Sudagar <66852579+Rohan-Sudagar@users.noreply.github.com>
2022-06-25 13:05:37 -1000

    Add files via upload

---
+++ Recipe.cs
@@ -0,0 +1,21 @@
+﻿namespace CookingRecipes.Models
+{
+    public class Recipe
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+        public string RecipeUrl { get; set; }
+        public string Category { get; set; }
+
+        public Recipe(string name, string description, string imageurl, string recipeurl, string category)
+        {
+            Name = name;
+            Description = description;
+            ImageUrl = imageurl;
+            RecipeUrl = recipeurl;
+            Category = category;
+        }
+
+    }
+}
